ea3387d6201137046f78d2a3c66eacd1c89ec609
orthwade <sema_229@protonmail.com>
2022-10-21 03:07:18 +0600

    compiling shaders

---
+++ src/shader/compile.bat
@@ -0,0 +1,2 @@
+glslc -fshader-stage=vertex ./vertex.glsl -o ./vertex.spv
+glslc -fshader-stage=fragment ./fragment.glsl -o ./fragment.spv
---
+++ src/shader/fragment.glsl
@@ -0,0 +1,8 @@
+#version 450
+
+layout (location = 0) out vec4 vec4_out_colour_;
+
+void main()
+{
+	vec4_out_colour_ = vec4(0.7f, 0.3f, 0.3f, 1.0f);
+}
